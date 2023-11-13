03918477f4d9ac8e1456601544de64e605eb10a3
zorindmitry <zorindk@gmail.com>
2023-11-13 22:30:27 +0200

    add hm

--- homework.sh
+++ homework.sh
@@ -24,6 +24,7 @@ vim README.md
 git add .
 git commit -m "first commit"
 
+#додаю файли з прошлих лекцій
 cp ../lecture2\ vm:vagrant/Vagrantfile .
 cp ../lecture2\ vm:vagrant/script.sh .
 cp ../lecture2\ vm:vagrant/database.sh .
@@ -31,32 +32,38 @@ git add .
 git commit -m "add Vagrantfile, script.sh database.sh"
 git push -u origin main
 
+#додаю локально 2 гілки
 git branch feature-1
 git branch feature-2
 
+#переходжу на гілку 1
 git checkout feature-1
-open . script.sh
-#add comment to script.sh
+#додаю зміни в script.sh
+vim script.sh
+
+#роблю комміт пуш і мердж з main
 git status
 git add script.sh
 git commit -m "feature 1 add comment"
 git push origin feature-1
-
 git checkout main
 git merge feature-1
 git push origin main
 
-
+#переходжу на гілку 2
 git checkout feature-2
+#додаю зміни в script.sh на тій же строчці
 vim script.sh
-#add other comments
+#роблю комміт пуш
 git add script.sh
 git commit -m "feature-2 merge test"
 git push origin feature-2
 git checkout main
+#виконую мердж, вирішую проблеми і обєдную в один комміт
 git merge --squash feature-2
 git log
 
+############
 #Author: zorindmitry <zorindk@gmail.com>
 #Date:   Mon Nov 13 21:41:14 2023 +0200
 
@@ -67,3 +74,7 @@ git log
 #    Date:   Mon Nov 13 20:34:06 2023 +0200
 
 #        feature-2 merge test
+
+###########
+
+#виконую зміни в feature-1
